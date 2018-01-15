c468cd6ba02f21d9fa6cfe3d9032a1d9b81f3bf0
Allefe <allefejose@hotmail.com>
2018-01-15 14:13:29 -0200

    Em Desenvolvimento

---
+++ src/main/java/com/peixoto/ramais/RamaisPeixotoApplication.java
@@ -0,0 +1,12 @@
+package com.peixoto.ramais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamaisPeixotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RamaisPeixotoApplication.class, args);
+	}
+}
